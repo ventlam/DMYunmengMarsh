1094d6dc288c5167cf5f3fe093142455a151b858
ventlam <ventlcc@gmail.com>
2013-10-17 15:42:43 +0800

    generate random seed.

---
+++ src/main/java/com/amap/dataplatform/bi/common/RawDataRandomSpilt.java
@@ -0,0 +1,26 @@
+package com.amap.dataplatform.bi.common;
+
+import java.util.Random;
+
+import com.amap.dataplatform.bi.recommend.PredicAccurateCal;
+
+public class RawDataRandomSpilt {
+	
+	/*
+	 * 生成伪随机种子，用于均匀切分数据集
+	 */
+	public static int genrateRanWeed()
+	{
+		long seed = 10248964;
+		int range = 10 ;
+		Random rweed = new Random(seed);
+		int rand = rweed.nextInt(range);
+		return rand;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RawDataRandomSpilt.genrateRanWeed();
+		
+	}
+
+}
